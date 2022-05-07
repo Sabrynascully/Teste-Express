447e5f370391444645733b14442b54d2afb64611
Sabrina Marques <scullysabrina8@gmail.com>
2022-05-07 20:10:16 -0300

    atualização 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 var cors = require('cors')
 
-var dados = require('./dbprodutos')
+var dados = require('./db')
 
 
 app.use(express.urlencoded({ extended: false }))
